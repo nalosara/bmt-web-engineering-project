8baf313770cd7211d93b91e2dbff01c63e1f767e
nalosara <sara.nalo@stu.ibu.edu.ba>
2023-10-09 23:44:29 +0200

    initial commit

---
+++ src/main/java/ba/ibu/edu/bemytech/BemytechApplication.java
@@ -0,0 +1,13 @@
+package ba.ibu.edu.bemytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemytechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BemytechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ba/ibu/edu/bemytech/BemytechApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.ibu.edu.bemytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemytechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
