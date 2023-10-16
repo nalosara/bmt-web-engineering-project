8bfda15e125ddf1ebc4373c5adbc43818b86adb6
nalosara <sara.nalo@stu.ibu.edu.ba>
2023-10-16 22:38:45 +0200

    UserController added

--- src/main/java/ba/ibu/edu/bemytech/rest/controllers/UserController.java
+++ src/main/java/ba/ibu/edu/bemytech/rest/controllers/UserController.java
@@ -1,4 +1,30 @@
 package ba.ibu.edu.bemytech.rest.controllers;
 
+import ba.ibu.edu.bemytech.core.model.User;
+import ba.ibu.edu.bemytech.core.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable int id) {
+        return userService.findById(id);
+    }
 }
