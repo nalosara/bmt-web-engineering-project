c28a8b132b94985a26c05b4e55ad84120eb0526b
nalosara <sara.nalo@stu.ibu.edu.ba>
2023-10-16 22:59:49 +0200

    ProductController added

---
+++ src/main/java/ba/ibu/edu/bemytech/rest/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package ba.ibu.edu.bemytech.rest.controllers;
+
+import ba.ibu.edu.bemytech.core.model.Product;
+import ba.ibu.edu.bemytech.core.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable int id) {
+        return productService.findById(id);
+    }
+}
