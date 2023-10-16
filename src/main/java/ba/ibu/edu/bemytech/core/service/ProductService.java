cfe1d9a504bc49ebd19933e8dfcaafa500069f7c
nalosara <sara.nalo@stu.ibu.edu.ba>
2023-10-16 22:55:18 +0200

    ProductService added

---
+++ src/main/java/ba/ibu/edu/bemytech/core/service/ProductService.java
@@ -0,0 +1,25 @@
+package ba.ibu.edu.bemytech.core.service;
+
+import ba.ibu.edu.bemytech.core.model.Product;
+import ba.ibu.edu.bemytech.core.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(@PathVariable int id) {
+        return productRepository.findById(id);
+    }
+}
