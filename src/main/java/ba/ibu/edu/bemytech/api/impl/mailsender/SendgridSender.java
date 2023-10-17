2a1aa6db51feb7fe2a1eaab44c66239c5060ad71
nalosara <sara.nalo@stu.ibu.edu.ba>
2023-10-17 11:33:35 +0200

    MailgunSender and SendgridSender implementation added

---
+++ src/main/java/ba/ibu/edu/bemytech/api/impl/mailsender/SendgridSender.java
@@ -0,0 +1,19 @@
+package ba.ibu.edu.bemytech.api.impl.mailsender;
+
+import ba.ibu.edu.bemytech.core.api.mailsender.MailSender;
+import ba.ibu.edu.bemytech.core.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SendgridSender implements MailSender {
+    @Override
+    public String send(List<User> users, String message) {
+        for(User user : users) {
+            System.out.println("Message sent to " + user.getEmailAddress());
+        }
+
+        return "Message " + message + " | Sent via Sendgrid.";
+    }
+}
