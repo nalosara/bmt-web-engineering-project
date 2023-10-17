bb9a895470ef1cd7148bc7d05a0cf43bd8671d1c
nalosara <sara.nalo@stu.ibu.edu.ba>
2023-10-17 10:50:20 +0200

    OrderService added

---
+++ src/main/java/ba/ibu/edu/bemytech/core/service/OrderService.java
@@ -0,0 +1,25 @@
+package ba.ibu.edu.bemytech.core.service;
+
+import ba.ibu.edu.bemytech.core.model.Order;
+import ba.ibu.edu.bemytech.core.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(@PathVariable int id) {
+        return orderRepository.findById(id);
+    }
+}
