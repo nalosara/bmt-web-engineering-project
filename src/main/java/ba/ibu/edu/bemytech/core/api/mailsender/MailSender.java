866fd1e63e9f732c300cbeee4b8a1cd957eda09b
nalosara <sara.nalo@stu.ibu.edu.ba>
2023-10-17 11:17:49 +0200

    MailSender interface added

---
+++ src/main/java/ba/ibu/edu/bemytech/core/api/mailsender/MailSender.java
@@ -0,0 +1,11 @@
+package ba.ibu.edu.bemytech.core.api.mailsender;
+
+import ba.ibu.edu.bemytech.core.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface MailSender {
+    public String send(List<User> users, String message);
+}
