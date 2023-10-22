66a92f684e21d0bf74a5d7d0b0f4c179b3bca761
nalosara <sara.nalo@stu.ibu.edu.ba>
2023-10-22 17:37:59 +0200

    mailsender fixes

--- src/main/java/ba/ibu/edu/bemytech/api/impl/mailsender/SendgridSender.java
+++ src/main/java/ba/ibu/edu/bemytech/api/impl/mailsender/SendgridSender.java
@@ -13,7 +13,7 @@ public class SendgridSender implements MailSender {
     @Override
     public String send(List<User> users, String message) {
         for(User user : users) {
-            System.out.println("Message sent to " + user.getEmailAddress());
+            System.out.println("Message sent to " + user.getEmail());
         }
 
         return "Message " + message + " | Sent via Sendgrid.";
